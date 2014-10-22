9ae43769dbc5d9f1d8c5bb884f2e3a5e102b463b
Justin Meyer <justinbmeyer@gmail.com>
2014-10-21 22:48:50 -0500

    updated with docs

--- components/home/home.js
+++ components/home/home.js
@@ -1,6 +1,18 @@
 var can = require('can/'),
 	homeStache = require('./home.stache!');
-	
+
+/**
+ * @module {function} components/home/ <sc-home>
+ * @parent steal-can-example
+ * 
+ * @signature `<sc-home>`
+ * 
+ * Creates the homepage
+ * 
+ * @body
+ * 
+ * 
+ */
 module.exports = can.Component.extend({
 	tag: "sc-home",
 	template: homeStache,
--- components/jquery-ui/jquery-ui.js
+++ components/jquery-ui/jquery-ui.js
@@ -3,6 +3,18 @@ import jqStache from './jquery-ui.stache!';
 import acc from 'ui/accordion';
 import $ from 'jquery';
 
+/**
+ * @module {function} components/jquery-ui/ <sc-jquery-ui>
+ * @parent steal-can-example
+ * 
+ * @signature `<sc-jquery-ui>`
+ * 
+ * Creates a jQueryUI demo.
+ * 
+ * @body
+ * 
+ * 
+ */
 export default can.Component.extend({
 	tag: "sc-jquery-ui",
 	template: jqStache,
